4881082d4a06547a670bba45e58b85f742746c35
Nicolas Grekas <nicolas.grekas@gmail.com>
2026-09-07 17:23:48 +0200

    [JsonPath][FrameworkBundle] Add JsonPathBundle
    (cherry picked from commit 6b2e4565a9407496ea15a8f46cd1b2b182fbaee8)
    (cherry picked from commit 1d919d70085a5d701e15e9d4d41b436dc1ff897c)

---
+++ DependencyInjection/JsonPathPass.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\Component\JsonPath\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\JsonPath\FunctionReturnType;
+
+/**
+ * Collects metadata (arity, return type) for custom JsonPath functions
+ * and injects it into the json_path.crawler service.
+ */
+class JsonPathPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container): void
+    {
+        if (!$container->hasDefinition('json_path.crawler')) {
+            return;
+        }
+
+        $metadata = [];
+        foreach ($container->findTaggedServiceIds('json_path.function') as $tags) {
+            foreach ($tags as $attributes) {
+                if (!isset($attributes['name'])) {
+                    continue;
+                }
+
+                $returnType = $attributes['return_type'] ?? null;
+
+                $metadata[$attributes['name']] = [
+                    'arity' => $attributes['arity'] ?? null,
+                    'return_type' => \is_string($returnType) ? FunctionReturnType::from($returnType) : $returnType,
+                ];
+            }
+        }
+
+        $container->getDefinition('json_path.crawler')->setArgument(1, $metadata);
+    }
+}
